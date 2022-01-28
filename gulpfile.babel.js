9821dc1a9af80a9cab221a255c76e1b48d244a67
wisheee <wishee.u@gmail.com>
2022-01-28 15:46:04 +0900

    chore: gulp sass 설정 추가

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,10 @@ import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
 import gimage from "gulp-image";
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+
+const sass = gulpSass(dartSass);
 
 const routes = {
 	pug: {
@@ -13,6 +17,11 @@ const routes = {
 	img: {
 		src: 'src/images/*',
 		dest: 'build/images'
+	},
+	scss: {
+		watch: 'src/scss/**/*.scss',
+		src: 'src/scss/style.scss',
+		dest: 'build/css'
 	}
 };
 
@@ -38,14 +47,21 @@ const img = () =>
 		.pipe(gimage())
 		.pipe(gulp.dest(routes.img.dest));
 
+const styles = () => 
+	gulp
+		.src(routes.scss.src)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest(routes.scss.dest));
+
 const watch = () =>	{
 	gulp.watch(routes.pug.watch, pug);
 	gulp.watch(routes.img.src, img);
+	gulp.watch(routes.scss.watch, styles);
 };
 
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, styles]);
 
 const postDev = gulp.parallel([webserver, watch]);
 
